319fbfbed8bcd9a69d973d96616e946dbc818936
William Reima <william.reima@gmail.com>
2023-10-17 20:39:32 +0300

    functional frontende server files

---
+++ frontend-server/tileserver.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, send_from_directory, send_file
+from flask_cors import CORS
+from waitress import serve
+
+app = Flask(__name__, static_folder='../frontend/dist/finland')
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/', defaults={'path' : ''})
+def server(path):
+    if path!= "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+
+
+
+if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=5000)
